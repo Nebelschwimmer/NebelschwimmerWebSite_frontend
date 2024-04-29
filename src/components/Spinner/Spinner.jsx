adbfd6097cd353eaf9919ed94c4f29be16131833
Nebelschwimmer <nebelschwimmer@gmail.com>
2024-04-29 13:00:43 +0300

    dfsdf

--- src/components/Spinner/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import CircularProgress from '@mui/material/CircularProgress';
-
-
-export const Spinner = () => {
-  return (
-      <CircularProgress fontSize='' color="inherit"/>
-  );
-}
